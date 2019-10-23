d4c27ed5b202f2c2c965b7d67c953ee0ba88ff4e
Piers Olenski <hello@piers.io>
2019-10-23 19:35:53 +0100

    Rename icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,7 +44,7 @@ module.exports = {
         background_color: THEME.colors.black,
         theme_color: THEME.colors.white,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`,
       },
     },
     {
