78103581f10083ba5a03832aab634007155e8902
Piers Olenski <hello@piers.io>
2019-11-13 22:57:12 +0000

    Simplify title

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,7 +6,7 @@ import SEO from '../components/common/Seo';
 export default function NotFoundPage() {
   return (
     <Layout>
-      <SEO title="404: Not found" />
+      <SEO title="Not found" />
       <h1>NOT FOUND</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     </Layout>
