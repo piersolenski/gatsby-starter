f8fdd1ec64365c682fdc9633b05e15370a3f80b8
Piers Olenski <hello@piers.io>
2020-02-05 13:57:13 +0000

    Update typography

--- src/components/common/Typography.js
+++ src/components/common/Typography.js
@@ -1,17 +1,37 @@
 import styled from 'styled-components';
 
+import fluidRange from '../../utils/fluidRange';
+
 export const H1 = styled.h1`
-  margin: 0 0 24px;
+  margin: 0 0 1.5rem;
+  line-height: 1.2em;
   font-weight: bold;
-  @media (max-width: 599px) {
-    font-size: 24px;
-  }
-  @media (min-width: 600px) {
-    font-size: 32px;
-  }
+  ${fluidRange({
+    prop: 'font-size',
+    fromSize: '41.89px',
+    toSize: '109.66px',
+  })};
+`;
+
+export const H2 = styled.h2`
+  margin: 0 0 1.5rem;
+  font-weight: bold;
+  ${fluidRange({
+    prop: 'font-size',
+    fromSize: '25.89px',
+    toSize: '41.89px',
+  })};
 `;
 
 export const P = styled.p`
-  font-size: 16px;
-  margin: 0 0 24px;
+  margin: 0 0 1.5rem;
+  line-height: 1.6em;
+  ${fluidRange({
+    prop: 'font-size',
+    fromSize: '16px',
+    toSize: '21px',
+  })};
+  &:last-child {
+    margin-bottom: 0;
+  }
 `;
