30213f0a9ea0cbe28d41c483dfa6cc212e82832f
Piers Olenski <hello@piers.io>
2020-02-05 15:34:14 +0000

    Add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 import Layout from '../components/common/Layout';
 import SEO from '../components/common/Seo';
+import Main from '../components/404/Main';
 
 export default function NotFoundPage() {
   return (
     <Layout>
       <SEO title="Not found" />
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <Main />
     </Layout>
   );
 }
