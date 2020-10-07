c7bd1d4616c9d2476950f11b516942fbb626de08
Piers Olenski <hello@piers.io>
2020-10-07 17:18:57 +0100

    Add default lang prop

--- src/components/common/Seo.js
+++ src/components/common/Seo.js
@@ -4,7 +4,22 @@ import { Helmet } from 'react-helmet';
 import { useLocation } from '@reach/router';
 import { useStaticQuery, graphql } from 'gatsby';
 
-const SEO = ({ title, description, image, article }) => {
+const query = graphql`
+  query SEO {
+    site {
+      siteMetadata {
+        defaultTitle: title
+        titleTemplate
+        defaultDescription: description
+        siteUrl
+        defaultImage: image
+        twitterUsername
+      }
+    }
+  }
+`;
+
+export default function SEO({ title, description, image, lang }) {
   const { pathname } = useLocation();
   const { site } = useStaticQuery(query);
   const {
@@ -22,11 +37,16 @@ const SEO = ({ title, description, image, article }) => {
     url: `${siteUrl}${pathname}`,
   };
   return (
-    <Helmet title={seo.title} titleTemplate={titleTemplate}>
+    <Helmet
+      htmlAttributes={{
+        lang,
+      }}
+      title={seo.title}
+      titleTemplate={titleTemplate}
+    >
       <meta name="description" content={seo.description} />
       <meta name="image" content={seo.image} />
       {seo.url && <meta property="og:url" content={seo.url} />}
-      {(article ? true : null) && <meta property="og:type" content="article" />}
       {seo.title && <meta property="og:title" content={seo.title} />}
       {seo.description && (
         <meta property="og:description" content={seo.description} />
@@ -43,35 +63,18 @@ const SEO = ({ title, description, image, article }) => {
       {seo.image && <meta name="twitter:image" content={seo.image} />}
     </Helmet>
   );
-};
-
-export default SEO;
+}
 
 SEO.propTypes = {
   title: PropTypes.string,
   description: PropTypes.string,
+  lang: PropTypes.string,
   image: PropTypes.string,
-  article: PropTypes.bool,
 };
 
 SEO.defaultProps = {
   title: null,
   description: null,
+  lang: 'en',
   image: null,
-  article: false,
 };
-
-const query = graphql`
-  query SEO {
-    site {
-      siteMetadata {
-        defaultTitle: title
-        titleTemplate
-        defaultDescription: description
-        siteUrl
-        defaultImage: image
-        twitterUsername
-      }
-    }
-  }
-`;
