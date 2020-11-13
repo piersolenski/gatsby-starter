4d3446d980b30d33413d91f053f069652c2c745f
Piers Olenski <hello@piers.io>
2020-11-13 23:57:53 +0000

    Remove margins

--- src/components/common/Typography.js
+++ src/components/common/Typography.js
@@ -3,29 +3,23 @@ import styled from 'styled-components';
 import lock from '../../utils/lock';
 
 export const H1 = styled.h1`
-  margin: 0 0 1.5rem;
+  ${lock('font-size', '32px', '62px')};
   line-height: 1.2em;
   font-weight: bold;
-  ${lock('font-size', '32px', '62px')};
 `;
 
 export const H2 = styled.h2`
-  margin: 0 0 1.5rem;
+  ${lock('font-size', '19px', '24px')};
   font-weight: bold;
-  ${lock('font-size', '21px', '24px')};
+  line-height: 1.4em;
 `;
 
 export const P = styled.p`
-  margin: 0 0 1.5rem;
-  line-height: 1.6em;
   ${lock('font-size', '15px', '17px')};
-  &:last-child {
-    margin-bottom: 0;
-  }
+  line-height: 1.6em;
 `;
 
 export const Small = styled.p`
-  line-height: 1.6em;
-  margin: 0;
   ${lock('font-size', '13px', '15px')};
+  line-height: 1.5em;
 `;
