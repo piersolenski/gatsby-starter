4e912c2ff9708adf69ff296f3453a32acbd85113
Piers Olenski <hello@piers.io>
2020-11-15 19:58:49 +0000

    Move duplicate data into package.json

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
 import Layout from 'components/common/Layout';
 import SEO from 'components/common/Seo';
 import Hero from 'components/common/Hero';
@@ -7,15 +8,12 @@ import Button from 'components/common/Button';
 import { H1 } from 'components/common/Typography';
 import Main from 'components/home/Main';
 
-export default function Index() {
+export default function Index({ data }) {
   return (
     <Layout>
-      <SEO
-        title="Home"
-        description="A simple starter project to get up and developing quickly with Gatsby."
-      />
+      <SEO title="Home" description={data.site.siteMetadata.description} />
       <Hero>
-        <H1>Dolor sociosqu curae mi torquent a phasellus class primis.</H1>
+        <H1>{data.site.siteMetadata.description}</H1>
         <Button size="large" color="black" to="/about">
           Read more
         </Button>
@@ -24,3 +22,17 @@ export default function Index() {
     </Layout>
   );
 }
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        description
+      }
+    }
+  }
+`;
+
+Index.propTypes = {
+  data: PropTypes.object.isRequired,
+};
