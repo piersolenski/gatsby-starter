198aa70597f3e4291991acc382f2529470cfd9d9
Piers Olenski <hello@piers.io>
2020-11-15 11:19:30 +0000

    Larger H1 with smaller letter spacing

--- src/components/common/Typography.js
+++ src/components/common/Typography.js
@@ -2,9 +2,10 @@ import styled from 'styled-components';
 import lock from 'utils/lock';
 
 export const H1 = styled.h1`
-  ${lock('font-size', '32px', '62px')};
+  ${lock('font-size', '32px', '72px')};
   line-height: 1.2em;
   font-weight: bold;
+  letter-spacing: -0.025em;
 `;
 
 export const H2 = styled.h2`
